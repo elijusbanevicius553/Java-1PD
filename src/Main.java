0c7ca3bea3c1e78d2dcae4e73eb11aa225f2416e
Elijus <elijuzs@gmail.com>
2023-03-18 01:09:00 +0200

    Iškviesti pair pagal pavyzdį

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("QQQQ");
+        Pair<Integer, String> pair = new Pair<>(10, "Hello");
+        System.out.println(pair.getFirst());    // Output: 10
+        System.out.println(pair.getSecond());   // Output: Hello
+        pair.setFirst(20);
+        pair.setSecond("World");
+        System.out.println(pair.getFirst());    // Output: 20
+        System.out.println(pair.getSecond());   // Output: World
     }
 }
