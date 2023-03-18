dfe56f35ec24bf115fa85f1d9a2fe7432b746a4d
Elijus <elijuzs@gmail.com>
2023-03-18 21:34:44 +0200

    Sukurta klasė Automobilis ir ji aprašyta. Comparable panaudojimas

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Pair<Integer, String> pair = new Pair<>(10, "Hello");
@@ -7,5 +8,30 @@ public class Main {
         pair.setSecond("World");
         System.out.println(pair.getFirst());    // Output: 20
         System.out.println(pair.getSecond());   // Output: World
+        List<Automobilis> automobiliuSarasas = new ArrayList<>();
+        automobiliuSarasas.add(new Automobilis("Audi", 2023));
+        automobiliuSarasas.add(new Automobilis("BMW", 2024));
+        automobiliuSarasas.add(new Automobilis("Mercedes", 2022));
+        automobiliuSarasas.add(new Automobilis("Tesla", 2025));
+
+        // Rūšiavimas pagal metus (Comparator)
+        automobiliuSarasas.sort(Comparator.comparing(Automobilis::getMetai));
+        System.out.println("Automobiliai pagal metus:");
+        automobiliuSarasas.forEach(System.out::println);
+
+        // Rūšiavimas pagal markę (Comparable)
+        Collections.sort(automobiliuSarasas);
+        System.out.println("\nAutomobiliai pagal markę:");
+        automobiliuSarasas.forEach(System.out::println);
+
+        // Rūšiavimas pagal markę atvirkštine tvarka (Comparator)
+        automobiliuSarasas.sort(Comparator.comparing(Automobilis::getMarke).reversed());
+        System.out.println("\nAutomobiliai pagal markę atvirkštine tvarka:");
+        automobiliuSarasas.forEach(System.out::println);
+
+        // Rūšiavimas pagal metus atvirkštine tvarka (Comparator)
+        automobiliuSarasas.sort(Comparator.comparing(Automobilis::getMetai).reversed());
+        System.out.println("\nAutomobiliai pagal metus atvirkštine tvarka:");
+        automobiliuSarasas.forEach(System.out::println);
     }
 }
